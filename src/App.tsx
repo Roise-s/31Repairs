00bab3281859d60d3c63b2d17da6f2b4151e0ad8
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-09-03 23:12:31 +0200

    starting run

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router";
 import './App.css'
+import Home from "./pages/Home";
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   )
